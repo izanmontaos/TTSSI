934a3383740785eb83fa8e096e9685571fd4963f
izanm <izan.montaos@udc.es>
2024-11-16 13:00:21 +0100

    Rutas Actualizadas

--- app/src/main/java/com/grovylessj/wifiscanner/WifiScannerApp.kt
+++
@@ -1,6 +0,0 @@
-package com.grovylessj.wifiscanner
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class WifiScannerApp:Application()
--- app/src/main/java/com/grovylessj/wifiscanner/data/NetworkAnalysisRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.grovylessj.wifiscanner.data
-
-import javax.inject.Inject
-
-class NetworkAnalysisRepository @Inject constructor(
-    private val networkAnalysisService: NetworkAnalysisService
-) {
-    fun getNetworkDetails() = networkAnalysisService.getNetworkDetails()
-}
